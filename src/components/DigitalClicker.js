a6b6d7df72995987ad11ef59bbcd933e2146037d
mistermur <brian.j.murillo@gmail.com>
2019-03-13 17:24:20 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,20 @@
 // Code DigitalClicker Component Here
+import React from "react";
+
+class DigitalClicker extends React.Component {
+  state={
+    timesClicked:0
+  }
+  handleOnClick=()=>{
+    this.setState({timesClicked:++this.state.timesClicked})
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleOnClick}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+export default DigitalClicker
